cdbf6520955bc9ea173ec2dbabfb64079308f776
Nayan Agrawal <nayan.agrawal@tech9.com>
2025-06-13 15:30:45 +0530

    added new folder

---
+++ tests/testData.ts
@@ -0,0 +1,14 @@
+export const testData = {
+    qa: `https://dev.gosmartdealer.com/login`,
+    dev: `https://dev.gosmartdealer.com/login`,
+    qaApi: `https://reqres.in`,
+    devApi: ``,
+    username: `dev-superadmin.dgs@yopmail.com`,
+    password: `Hello1234`,
+    waitForElement: 120000,
+    dbUsername: ``,
+    dbPassword: ``,
+    dbServerName: ``,
+    dbPort: ``,
+    dbName: ``
+}
