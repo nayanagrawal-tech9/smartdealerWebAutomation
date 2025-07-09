80752b9badcd873364c1dee7be2852f1f0e6e5ad
Nayan Agrawal <nayan.agrawal@tech9.com>
2025-07-09 16:08:26 +0530

    added playwright changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,52 +1,53 @@
-const {  devices } = require('@playwright/test');
+const { devices } = require('@playwright/test');
 
 const config = {
   testDir: './tests',
-  retries : 0, //Number of retries for failed test cases
-  Worker : 1, //Running parallel execution
-  timeout : 40 * 1000,
+  retries: 0, //Number of retries for failed test cases
+  Worker: 1, //Running parallel execution
+  timeout: 40 * 1000,
   expect: {
-    timeout : 50000
+    timeout: 50000
   },
-  
+
   reporter: [
     ['list'],
-    ['junit', { outputFile: 'results/test-results.xml' }]
-    ['allure-playwright']    
+    ['junit', { outputFile: 'results/test-results.xml' }],
+    ['allure-playwright']
   ],
   //reporter: 'html',
-  projects : [
+  projects: [
     {
-      name : 'Safari',
+      name: 'Safari',
       use: {
-  
-        browserName : 'webkit',
-        headless : false,
-        screenshot : 'on',
-        trace : 'on',
-        viewport : null,
-        ignoreHttpsErrors : true,
-        Permissions:['geolocation'],
-        video : 'retain-on-failure'
+
+        browserName: 'webkit',
+        headless: false,
+        screenshot: 'on',
+        trace: 'on',
+        viewport: null,
+        ignoreHttpsErrors: true,
+        Permissions: ['geolocation'],
+        video: 'retain-on-failure'
         //viewport : {width:123, height:123},
         //...devices['iPhone 11']
-      }},
-      {
-        name : 'Chrome',
-        use: {
-    
-          browserName : 'chromium',
-          headless : true,
-          screenshot : 'on',
-          trace : 'on',
-          viewport : null,
-          ignoreHttpsErrors : true,
-          Permissions:['geolocation'],
-          video : 'retain-on-failure'
-        }
+      }
+    },
+    {
+      name: 'Chrome',
+      use: {
+
+        browserName: 'chromium',
+        headless: true,
+        screenshot: 'on',
+        trace: 'on',
+        viewport: null,
+        ignoreHttpsErrors: true,
+        Permissions: ['geolocation'],
+        video: 'retain-on-failure'
+      }
     }
 
-    
+
   ],
 };
 module.exports = config
