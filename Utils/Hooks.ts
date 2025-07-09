c0b30eaf1a9b02b266faa8777dcac540ded669a2
Nayan Agrawal <nayan.agrawal@tech9.com>
2025-07-09 15:21:18 +0530

    added playwright changes

--- Utils/Hooks.ts
+++ Utils/Hooks.ts
@@ -1,25 +1,16 @@
-import { AfterStep, Before, Status } from "@cucumber/cucumber";
-import playwright from '@playwright/test'
-import { chromium } from 'playwright';
-import { POManager } from "../PageObjects/POManager";
-import { CustomWorld } from './World';
 import fs from 'fs';
+import { afterEach } from "node:test";
+import path from 'path';
 
-Before(async function (this: CustomWorld) {
-
-    const browser = await chromium.launch({ headless: false });
-    const context = await browser.newContext();
-    this.page = await context.newPage();
-    this.poManager = new POManager(this.page);
-})
-
-AfterStep(async function ({ result }) {
-
-    if (result?.status === Status.FAILED) {
-        if (!fs.existsSync('screenshots')) {
-            fs.mkdirSync('screenshots');
-        }
-        await this.page.screenshot({ path: `screenshots/failure-${Date.now()}.png` });
+afterEach(async function () {
+    if (this.currentTest?.state === 'failed') {
+      const screenshotsDir = path.resolve('screenshots');
+      if (!fs.existsSync(screenshotsDir)) {
+        fs.mkdirSync(screenshotsDir, { recursive: true });
+      }
+  
+      const filePath = `${screenshotsDir}/failure-${Date.now()}.png`;
+      await this.page.screenshot({ path: filePath });
+      console.log(`📷 Screenshot saved to: ${filePath}`);
     }
-
-})
+  });
